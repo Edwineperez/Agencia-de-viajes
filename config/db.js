0b19b749920ddfce38db9deb1c060c8a9097fccf
Edwin Pérez <145898405+Edwineperez@users.noreply.github.com>
2025-05-28 10:33:47 -0500

    Add files via upload

--- config/db.js
+++ config/db.js
@@ -1,20 +1,23 @@
-import Sequelize from 'sequelize';
+import { Sequelize } from 'sequelize';
+import dotenv from 'dotenv';
 
-const db = new Sequelize('agenciadeviajes', 'root', '990204', {
-    host: '127.0.0.1',
-    port: '3306',
-    dialect: 'mysql',
-    define: {
-        timestamps: false
-    },
-    pool: {
-        max: 5,
-        main: 0,
-        acquire: 30000,
-        idle: 10000
-    },
-    operatorAlisases: false
+dotenv.config();
 
-});
+const db = new Sequelize('brf3sl5rdgxso6tf8ztk', 'utjupkhzp1gmaerq', 'kliZXPLZVpeX7hQrEJI3', {
+        host: '127.0.0.1',
+        port: '3306',
+        dialect: 'mysql',
+        define: {
+            timestamps: false
+        },
+        pool: {
+            max: 5,
+            min: 0,
+            acquire: 30000,
+            idle: 10000
+        },
+        operatorsAliases: false
+    }
+);
 
 export default db;
